ae9cb960b1303d5b3cd4e84dc3a0b37983fe06ac
Felipe José <felipej0se@icloud.com>
2021-08-12 20:24:44 -0300

    ffix: cors

--- back/src/main.ts
+++ back/src/main.ts
@@ -4,9 +4,19 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  var whitelist = ['https://4pets.company', 'https://www.4pets.company'];
+
   app.setGlobalPrefix('api/v1');
   app.enableCors({
-    origin: true,
+    origin: function (origin, callback) {
+      if (whitelist.indexOf(origin) !== -1) {
+        console.log('allowed cors for:', origin);
+        callback(null, true);
+      } else {
+        console.log('blocked cors for:', origin);
+        callback(new Error('Not allowed by CORS'));
+      }
+    },
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     credentials: true,
   });
