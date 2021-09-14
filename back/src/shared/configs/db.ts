eff4acdbc0227a2226c73f4bd75af223d8eb0177
Felipe José <felipej0se@icloud.com>
2021-09-14 18:59:10 -0300

    feat: adjust infra imports

--- back/src/shared/configs/db.ts
+++ back/src/shared/configs/db.ts
@@ -6,15 +6,15 @@ dotenv.config();
 export const databaseConfig: IDatabaseConfig = {
   staging: {
     dialect: 'sqlite',
-    storage: './src/infra/database/staging.sqlite',
+    storage: './infra/database/staging.sqlite',
   },
   test: {
     dialect: 'sqlite',
-    storage: './src/infra/database/test.sqlite',
+    storage: './infra/database/test.sqlite',
     logging: false,
   },
   production: {
     dialect: 'sqlite',
-    storage: './src/infra/database/production.sqlite',
+    storage: './infra/database/production.sqlite',
   },
 };
